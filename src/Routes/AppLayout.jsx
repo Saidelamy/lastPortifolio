d54053210bf80b9c20f28b6031aa2c156116ff44
Saidelamy <saidmagdy511@gmail.com>
2024-09-20 14:26:56 +0300

    update footer from AppLayout

--- src/Routes/AppLayout.jsx
+++ src/Routes/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../componants/Navbar';
-import Footer from '../Ui/footer';
+import Footer from '../Ui/Footer';
 
 const AppLayout = () => {
   return (
