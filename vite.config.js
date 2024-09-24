cb66feb540944d5c6e0dc76ace122edfca6b6329
Saidelamy <saidmagdy511@gmail.com>
2024-09-24 21:41:55 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.JPG"],
+    build: {
+    rollupOptions: {
+      external: ['module-name'],
+    },
+  },
 });
