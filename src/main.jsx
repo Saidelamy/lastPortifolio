397ee8a3c70f8d6becb562551f9898ac4420612a
Saidelamy <saidmagdy511@gmail.com>
2024-09-30 12:58:57 +0300

    update paricle

--- src/main.jsx
+++ src/main.jsx
@@ -5,22 +5,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
-// import Particle from './componants/Particle.jsx';
-// import { Helmet } from 'react-helmet';
+import Particle from './componants/Particle.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      {/* <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet> */}
       <App />
-      {/* <Particle id="particle" /> */}
+      <Particle id="particle" />
       <ToastContainer
         position="top-right"
         autoClose={5000}
